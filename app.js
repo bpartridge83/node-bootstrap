0e1365f28b06221a2dfc1fd90673ce3f7020cb74
Brian Partridge <bpartridge@Brians-MacBook-Air-2.local>
2013-03-08 14:36:32 -0800

    Adding assets

--- app.js
+++ app.js
@@ -4,6 +4,42 @@ var express = require('express'),
 	http = require('http'),
 	server = http.createServer(app);
 	
+var _ = require('underscore'),
+	Deferred = require('deferred'),
+	Async = require('async');
+	
+var cons = require('consolidate'),
+	swig = require('swig'),
+	mongo = require('mongoskin');
+	
+	
+swig.init({
+    root: __dirname + '/views',
+    allowErrors: true
+});
+	
+app.configure(function () {
+
+	//app.use(express.favicon(__dirname + '/public/favicon.ico', {maxAge: 86400000}));
+    app.use(express.static(__dirname + '/public'));
+
+	app.use(express.compress());
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+				
+	app.engine('.html.twig', cons.swig);
+	app.set('view engine', 'html.twig');
+	app.set('views', __dirname + '/views');
+    app.set("view options", { layout: false });
+
+});
+	
+app.get('/', function (req, res) {
+
+	res.render('index');
+	
+});
+	
 app.get('/status', function (req, res) {
 	
 	var pjson = require('./package.json');
