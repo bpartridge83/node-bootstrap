8d969365657baa58993ded604ee9f4c1f5fe8a44
Brian Partridge <bpartridge@Brians-MacBook-Air-2.local>
2013-03-08 14:20:13 -0800

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express'),
+	app = express(),
+	argv = require('optimist').argv,
+	http = require('http'),
+	server = http.createServer(app);
+	
+app.get('/status', function (req, res) {
+	
+	var pjson = require('./package.json');
+	
+	return res.send({
+		status: 'running',
+		environment: app.get('site'),
+		version: pjson.version
+	});
+	
+});
+	
+var portscanner = require('portscanner'),
+	port_range = [5000, 5010];
+
+portscanner.findAPortNotInUse(port_range[0], port_range[1], 'localhost', function (error, _port) {
+
+	var port = argv.port || process.env.PORT || _port;
+
+	server.listen(port, function () {
+		console.log('Listening on Port ' + port);
+	});
+
+});
